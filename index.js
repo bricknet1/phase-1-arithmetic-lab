1dc9dcb775cf50d1f259758392beef34fcf48813
Nick Johnson <nlj222@gmail.com>
2023-01-09 19:59:50 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+let num1 = 62;
+let num2 = 1;
+let num3 = 19;
+let num4 = 5;
+let num5 = 20;
+let num6 = 2;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num3 % num4;
+
+let max = Math.max(num5, num6);
